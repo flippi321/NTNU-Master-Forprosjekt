7b38c2bf560e646409860348c1e273b199f14b58
Christoffer Brevik <chribrev@stud.ntnu.no>
2025-11-03 11:09:15 +0100

    Implemented a ssim_L1_2D_loss using SSIM, L1 and KL Divergence

--- utils/loss_functions.py
+++ utils/loss_functions.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+from pytorch_msssim import ssim, ms_ssim, SSIM, MS_SSIM
 
 # ---------------------------------------------
 # 2D Loss Functions
@@ -14,8 +15,19 @@ def binary_2d_loss(recon, target, mu, logvar):
     # KL divergence
     kld = -0.5 * torch.mean(1 + logvar - mu.pow(2) - logvar.exp())
     # Small KL weight for stability
-    return bce + 1e-3 * kld, bce, kld
+    return bce + 1e-3 * kld
 
+
+def ssim_L1_2d_loss(recon, target, mu, logvar):
+    # SSIM loss
+    ssim_loss = 1 - ssim(recon, target, data_range=1.0, size_average=True)
+    # L1 loss
+    l1_loss = nn.functional.l1_loss(recon, target, reduction='mean')
+    # KL divergence
+    kld = -0.5 * torch.mean(1 + logvar - mu.pow(2) - logvar.exp())
+
+    # Combine losses with weights
+    return ssim_loss + l1_loss + 1e-3 * kld
 
 # ---------------------------------------------
 # 3D Loss Functions
