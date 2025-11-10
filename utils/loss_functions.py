9b484972186aca3bb8eca784ae3e1c50ad65217a
Christoffer Brevik <chribrev@stud.ntnu.no>
2025-11-10 16:25:13 +0100

    Removed KL divergence and implemented a loss clip to hinder one loss from exploding average_loss value

--- utils/loss_functions.py
+++ utils/loss_functions.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import numpy as np
 from pytorch_msssim import ssim
 import torch.nn.functional as F
 
@@ -10,30 +11,23 @@ import torch.nn.functional as F
 # model reconstructions and compare them
 # ---------------------------------------------
 
-def clip_loss(loss: int, max:int =1e4) -> int:
-    """Clip loss to avoid extreme values destabilizing training."""
-    return min(loss, max)
-
-
-def binary_2d_loss(recon, target, mu, logvar):
+def clip_loss(loss: torch.Tensor, max: float = 1e6) -> torch.Tensor:
+    # Make sure loss does not explode
+    return torch.clamp(loss, max=max)
+def binary_2d_loss(recon: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
     # Reconstruction: BCE because inputs are in [0,1]
     bce = nn.functional.binary_cross_entropy(recon, target, reduction='mean')
     # KL divergence
-    kld = -0.5 * torch.mean(1 + logvar - mu.pow(2) - logvar.exp())
-    # Small KL weight for stability
-    return clip_loss(bce + 1e-3 * kld)
+    return clip_loss(bce)
 
-
-def ssim_L1_2d_loss(recon, target, mu, logvar):
+def ssim_L1_2d_loss(recon: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
     # SSIM loss
     ssim_loss = 1 - ssim(recon, target, data_range=1.0, size_average=True)
     # L1 loss
     l1_loss = nn.functional.l1_loss(recon, target, reduction='mean')
-    # KL divergence
-    kld = -0.5 * torch.mean(1 + logvar - mu.pow(2) - logvar.exp())
-
+    
     # Combine losses with weights
-    return clip_loss(ssim_loss + l1_loss + 1e-3 * kld)
+    return clip_loss(ssim_loss + l1_loss)
 
 # ---------------------------------------------
 # 3D Loss Functions
